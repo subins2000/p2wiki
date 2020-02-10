4b95335e7fa8030efefbd9dbd0ccb5e369430e51
athul <athul8720@gmail.com>
2020-02-10 09:57:34 +0530

    Better Styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,13 +7,13 @@ class Main extends Component {
     return (
       <>
       <div>
-      	<h1>P2Wiki</h1>
+      	<h1 className="font-mono text-6xl text-center">P2Wiki</h1>
         <Searchbar/>
       </div>
       <footer className="footer">
       <div className="content has-text-centered">
-        <p style={{color:`hotpink`}}>
-          P2Wiki made by Subin Siby, Athul Cyriac Ajay, Pranav Shridhar
+        <p className="font-mono text-1xl" style={{color:`hotpink`}}>
+          P2Wiki made by Subin Siby, Pranav Shridhar,Athul Cyriac Ajay @Hackcamp
         </p>
       </div>
     </footer>
